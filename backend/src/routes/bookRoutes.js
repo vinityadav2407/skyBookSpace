578afebd62b0449067c9201d84cf75785b69c48e
vinityadav2407 <vinityadav885317@gmail.com>
2026-05-03 20:37:51 +0530

    backenddone

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -2,10 +2,11 @@ import express from 'express';
 import cloudinary from '../lib/cloudinary.js';
 import User from '../models/User.js';
 import Book from '../models/Book.js';
+import protectAuth from '../middleware/auth_middleware.js';
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', protectAuth, async (req, res) => {
   try {
     const { title, caption, rating, image } = req.body;
 
@@ -33,6 +34,111 @@ router.post('/', async (req, res) => {
 
   } catch (error) {
     res.status(500).json({ message: error.message });
+  }
+});
+
+// get all books with pagination
+router.get('/', protectAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate('user', 'username profileImage');
+
+    const total = await Book.countDocuments();
+
+    res.status(200).json({
+      success: true,
+      books,
+      currentPage: page,
+      limit,
+      totalBooks: total,
+      totalPages: Math.ceil(total / limit), // ✅ FIXED
+    });
+
+  } catch (error) {
+    console.log('Error to get all Books',error)
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+    });
+  }
+});
+
+//delete route
+router.delete('/:id', protectAuth, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+
+    if (!book) {
+      return res.status(404).json({ message: 'Book not found' });
+    }
+
+    // Check ownership
+    if (book.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: 'Unauthorized' });
+    }
+
+    // ✅ Delete image from Cloudinary (safe extraction)
+    if (book.image && book.image.includes('cloudinary')) {
+      try {
+        const parts = book.image.split('/');
+        const fileName = parts.slice(-2).join('/'); // handles folders
+        const publicId = fileName.split('.')[0];
+
+        await cloudinary.uploader.destroy(publicId);
+      } catch (err) {
+        console.error('Cloudinary delete error:', err.message);
+      }
+    }
+
+    // ✅ Delete from DB
+    await book.deleteOne();
+
+    res.status(200).json({
+      success: true,
+      message: 'Book and image deleted successfully',
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error',
+      error: error.message,
+    });
+  }
+});
+
+// current login user book recomendation
+router.get('/user', protectAuth, async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const books = await Book.find({ user: req.user._id })
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const total = await Book.countDocuments({ user: req.user._id });
+
+    res.status(200).json({
+      success: true,
+      books,
+      currentPage: page,
+      totalBooks: total,
+      totalPages: Math.ceil(total / limit),
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
   }
 });
 
